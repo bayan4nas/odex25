7dc4f504b60288e401854b5b27b0041c294a38a4
Mazen Abdo <one6428300@gmail.com>
2025-01-01 10:11:40 +0200

    add module hr_trahum

---
+++ odex25_hr/hr_trahum/__manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "hr_trahum",
+
+    'summary': """
+        Short (1 phrase/line) summary of the module's purpose, used as
+        subtitle on modules listing or apps.openerp.com""",
+
+    'description': """
+        Long description of module's purpose
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/14.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base','hr_contract_custom','employee_requests','exp_ticket_request','exp_official_mission','exp_payroll_promotion'],
+
+    # always loaded
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/employee_overtime.xml',
+        'views/templates.xml',
+        'report/report.xml',
+        'data/sequence.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
