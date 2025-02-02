a2a31eace738d1b4d3e66345fbd6057296cdeafe
hasan-exp <h.salah@exp-sa.com>
2025-02-02 10:12:17 +0300

    edit forward_user

--- odex25_transactions/exp_transaction_documents/models/incoming_transaction.py
+++ odex25_transactions/exp_transaction_documents/models/incoming_transaction.py
@@ -149,8 +149,11 @@ class IncomingTransaction(models.Model):
                 })
                 partner_ids = []
                 if record.to_ids.type == 'unit':
-                    partner_ids.append(record.to_ids.secretary_id.user_id.partner_id.id)
-                    record.forward_user_id = record.to_ids.secretary_id.user_id.id
+                    for s in record.to_ids.secretary_ids : 
+                        partner_ids.append(s.user_id.partner_id.id)
+                        record.forward_entity_id = record.to_ids.id
+                    # record.forward_user_id = record.to_ids.secretary_id.user_id.id
+
                 elif record.to_ids.type == 'employee':
                     partner_ids.append(record.to_ids.user_id.partner_id.id)
                     record.forward_user_id = record.to_ids.user_id.id
