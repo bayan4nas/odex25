c2837396c74a49e82c996134b81d75f4c8df8456
hasan-exp <h.salah@exp-sa.com>
2025-02-03 07:17:45 +0300

    sovel reply_user_ids

--- odex25_transactions/exp_transaction_documents/wizard/transaction_reply_wizard.py
+++ odex25_transactions/exp_transaction_documents/wizard/transaction_reply_wizard.py
@@ -46,7 +46,12 @@ class TransactionReturnWizard(models.TransientModel):
         if user_id not in transaction.seen_user_ids.ids:
             transaction.seen_user_ids = [(6, 0, [user_id])]
         forward_entity = self.env['cm.entity'].search([('user_id', '=', transaction.forward_user_id.id)], limit=1)
-        transaction.current_entity_id = False
+        if transaction.current_entity_id :
+            if transaction.current_entity_id.type == 'employee' :
+                transaction.reply_user_ids = [(4, transaction.current_entity_id.user_id.id)]
+            else  :
+                transaction.reply_user_ids = [(4, s.user_id.id) for s in  transaction.current_entity_id.secretary_ids]
+            transaction.current_entity_id = False
         # Check if attachment_id and filename are not empty before creating attachment
         if self.attachment_id and self.filename:
             attachment = self.env['ir.attachment'].create({
