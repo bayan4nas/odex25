73ab2c73324ffc43ca0b4ed287e5af86d09fb173
hasan-exp <h.salah@exp-sa.com>
2025-02-04 13:38:18 +0300

    remove trans

---
+++ odex25_transactions1/exp_transaction_documents/models/outgoing_transaction.py
@@ -0,0 +1,192 @@
+# -*- coding: utf-8 -*-
+from odoo import models, api, fields, _
+from odoo.exceptions import ValidationError
+
+
+class OutgoingTransaction(models.Model):
+    _name = 'outgoing.transaction'
+    _inherit = ['transaction.transaction', 'mail.thread']
+    _description = 'outgoing Transaction'
+
+    reason = fields.Text('Reason')
+    transaction_type = fields.Selection([
+        ('incoming', 'Incoming'),
+        ('outgoing', 'Outgoing'),
+    ], string='Type')
+    attachment_rule_ids = fields.One2many('cm.attachment.rule', 'outgoing_transaction_id', string='Attaches')
+    attachment_ids = fields.One2many('cm.attachment', 'outgoing_transaction_id', string='Attachments')
+    trace_ids = fields.One2many('cm.transaction.trace', 'outgoing_transaction_id', string='Trace Log')
+    is_partner = fields.Boolean()
+    partner_id = fields.Many2one('res.partner')
+    incoming_transaction_id = fields.Many2one('incoming.transaction', string='Related Incoming')
+    # to_ids = fields.Many2one(comodel_name='cm.entity',string='Send To')
+    # to_delegate = fields.Boolean(string='To Delegate?', related='to_ids.to_delegate')
+    company_name = fields.Many2one('res.partner',string='Delivery Company')
+
+    to_users = fields.Many2many(comodel_name='res.users', string="To Users",relation='your_out_to_users_rel',column1='your_out_id',column2='user_id2',)
+    
+    
+
+    tran_tag = fields.Many2many(comodel_name='transaction.tag', string='Tags')
+    tran_tag_unit = fields.Many2many(comodel_name='transaction.tag', string='Business unit',
+                                     relation='outgoing_tag_rel',
+                                     column1='incoming_id'
+                                     , column2='name')
+    project_id = fields.Many2many('project.project')
+    sale_order_id = fields.Many2one('sale.order', 'Proposal')
+    to_name = fields.Char(string="Recipient")
+    cc_ids = fields.Many2many(comodel_name='cm.entity', relation='outgoing_entity_cc_rel',
+                              column1='outgoing_id', column2='entity_id', string='CC To')
+    cc_users = fields.Many2many(comodel_name='res.users', string="CC Users",relation='your_com_to_users_rel',column1='your_use_id',column2='user_id',store=True)
+
+    processing_ids = fields.Many2many(comodel_name='outgoing.transaction', relation='transaction_outgoing_outgoing_rel',
+                                      column1='transaction_id', column2='outgoing_id',
+                                      string='Process Transactions outgoing')
+    processing2_ids = fields.Many2many(comodel_name='incoming.transaction',
+                                       relation='transaction_outgoing_incoming_rel',
+                                       column1='transaction_id', column2='incoming_id',
+                                       string='Process Transactions incoming')
+
+    # processing_ids = fields.Many2many(comodel_name='transaction.transaction', relation='transaction_outgoing_rel',
+    #                                   column1='transaction_id', column2='outgoing_id', string='Process Transactions')
+
+    def _normalize_arabic_text(self, text):
+        translation_map = str.maketrans({
+            # Define a dictionary to replace different forms of characters
+            'ه': 'ة',
+            'إ': 'ا',  # Replace Alef with Hamza Below with Alef
+            'أ': 'ا',  # Replace Alef with Hamza Above with Alef
+            'آ': 'ا',  # Replace Alef with Madda Above with Alef
+            'ى': 'ي',  # Replace Alef Maqsura with Ya
+            'ئ': 'ي',
+            'ؤ': 'و',
+
+        })
+        return text.translate(translation_map)
+
+    @api.model
+    def search(self, args, offset=0, limit=None, order=None, count=False):
+        # Normalize the search arguments for 'name' field
+        new_args = []
+        for arg in args:
+            if isinstance(arg, (list, tuple)) and arg[1] == 'ilike' and arg[0] in ('name', 'subject'):
+                normalized_value = self._normalize_arabic_text(arg[2])
+                new_args.append('|')
+                new_args.append(arg)
+                new_args.append((arg[0], 'ilike', normalized_value))
+            else:
+                new_args.append(arg)
+        return super(OutgoingTransaction, self).search(new_args, offset=offset, limit=limit, order=order, count=count)
+
+
+    @api.depends('attachment_rule_ids')
+    def compute_attachment_num(self):
+        for r in self:
+            r.attachment_num = len(r.attachment_rule_ids)
+
+    @api.model
+    def get_url(self):
+        url = u''
+        action = self.env.ref(
+            'exp_transaction_documents.outgoing_external_tran_action', False)
+        Param = self.env['ir.config_parameter'].sudo()
+        if action:
+            return u'{}/web#id={}&action={}&model=outgoing.transaction'.format(
+                Param.get_param('web.base.url', self.env.user.company_id.website), self.id, action.id)
+        return url
+
+    def fetch_sequence(self, data=None):
+        """generate transaction sequence"""
+        return self.env['ir.sequence'].next_by_code('cm.transaction.out') or _('New')
+
+    ####################################################
+    # Business methods
+    ####################################################
+    #
+    #
+    def unlink(self):
+        for record in self:
+            if record.state != 'draft':
+                raise ValidationError(_('Cannot delete a sent transaction!'))
+        return super(InternalTransaction, self).unlink()
+
+    # def action_draft(self):
+    #     for record in self:
+    #         """her i need to review code for to_ids"""
+    #         # res = super(OutgoingTransaction, self).action_draft()
+    #         if record.subject_type_id.transaction_need_approve or record.preparation_id.need_approve:
+    #                 record.state = 'to_approve'
+    #         else:
+    #             record.state = 'complete'
+    #         # record.trace_create_ids('outgoing_transaction_id', record, 'sent')
+    #         partner_ids = [record.preparation_id.manager_id.user_id.partner_id.id]
+    #         subj = _('Message Has been send !')
+    #         msg = _(u'{} &larr; {}').format(record.employee_id.name, u' / '.join([k.name for k in record.to_ids]))
+    #         msg = u'{}<br /><b>{}</b> {}.<br />{}'.format(msg,
+    #                                                       _(u'Action Taken'), record.procedure_id.name,
+    #                                                       u'<a href="%s" >رابط المعاملة</a> ' % (
+    #                                                           record.get_url()))
+    #         self.action_send_notification(subj, msg, partner_ids)
+            # return res
+
+    
+    def action_draft(self):
+        for record in self:
+            """Here we need to review the code for to_ids"""
+            if record.subject_type_id.transaction_need_approve or record.preparation_id.need_approve:
+                record.state = 'to_approve'
+            else:
+                record.state = 'complete'
+    
+            partner_ids = [record.preparation_id.manager_id.user_id.partner_id.id]
+            
+            subj = _('Message Has been sent!')
+            msg = _(u'{} &larr; {}').format(record.employee_id.name, record.to_ids.name)
+            
+            msg = u'{}<br /><b>{}</b> {}.<br />{}'.format(msg,
+                                                          _(u'Action Taken'), record.procedure_id.name,
+                                                          u'<a href="%s" >رابط المعاملة</a>' % (
+                                                              record.get_url()))
+            self.action_send_notification(subj, msg, partner_ids)
+
+    def action_email(self):
+        # todo#add email function here
+        company_id = self.env.user.company_id
+        if company_id.sms_active == True:
+            test = company_id.send_sms("", "Test from odex!")
+            test = test.text[:100].split("-")
+            error = company_id.get_error_response(test[1])
+        for rec in self:
+            templates = 'exp_transaction_documents.out_email'
+            template = self.env.ref(templates, False)
+            emails = rec.partner_id.email if rec.is_partner else rec.to_ids.mapped('email')
+            email_template = template.write(
+                {'email_to': emails})
+            template.with_context(lang=self.env.user.lang).send_mail(
+                rec.id, force_send=True, raise_exception=False)
+
+    def action_reject_outgoing(self):
+        name = 'default_outgoing_transaction_id'
+        return self.action_reject(name, self)
+
+    def action_return_outgoing(self):
+        name = 'default_outgoing_transaction_id'
+        return self.action_return_tran(name, self)
+
+    ####################################################
+    # ORM Overrides methods
+    ####################################################
+    @api.model
+    def create(self, vals):
+        seq = self.fetch_sequence()
+        if vals.get('preparation_id'):
+            code = self.env['cm.entity'].sudo().browse(vals['preparation_id']).code
+            x = seq.split('/')
+            sequence = "%s/%s/%s" % (x[0], code, x[1])
+            vals['name'] = sequence
+        else:
+            vals['name'] = seq
+        # vals['ean13'] = self.env['odex.barcode'].code128('OT', vals['name'], 'TR')
+        return super(OutgoingTransaction, self).create(vals)
+
+   
