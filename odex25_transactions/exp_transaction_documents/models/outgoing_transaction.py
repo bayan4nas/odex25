ae20579473d08555d32faeb35abe280fd07a5448
esraa <e.mostafa@exp-sa.com>
2025-05-11 10:31:51 +0300

    add cm-subject

--- odex25_transactions/exp_transaction_documents/models/outgoing_transaction.py
+++ odex25_transactions/exp_transaction_documents/models/outgoing_transaction.py
@@ -99,6 +99,7 @@ class OutgoingTransaction(models.Model):
                 transaction.last_received_entity_id = last_track.to_id.id
                 transaction.last_sender_entity_id = last_track.from_id.id
                 transaction.last_sender_label = last_track.from_label
+                transaction.cm_subject = last_track.procedure_id
 
             else:
                 transaction.last_received_entity_id = False
