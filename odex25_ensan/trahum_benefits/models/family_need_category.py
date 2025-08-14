50d9a2952db64e97233cd9d5ad4f259f2f95b835
mohammed-alkhazrji <M.Alkhazrji@exp-sa.com>
2025-08-15 01:37:19 +0300

    need calc family

---
+++ odex25_ensan/trahum_benefits/models/family_need_category.py
@@ -0,0 +1,28 @@
+from odoo import models, fields, api
+from odoo.exceptions import ValidationError
+
+
+class FamilyNeedCategory(models.Model):
+    _name = 'family.need.category'
+    _description = 'Family Need Category Classification'
+
+    name = fields.Char(string="Family Need Category", required=True)
+    min_need = fields.Float(string="Minimum Need (%)", store=True)
+    max_need = fields.Float(string="Maximum Need (%)", store=True)
+
+    @api.constrains('min_need', 'max_need')
+    def _check_needs(self):
+        for rec in self:
+            if rec.min_need > rec.max_need:
+                raise ValidationError("Minimum need cannot be greater than maximum need")
+
+            overlapping = self.search([
+                ('id', '!=', rec.id),
+                ('min_need', '<=', rec.max_need),
+                ('max_need', '>=', rec.min_need),
+            ])
+            if overlapping:
+                raise ValidationError(
+                    _("This need range overlaps with existing categories: %s") %
+                    ', '.join(overlapping.mapped('name'))
+                )
