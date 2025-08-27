d0e516aab2956d8c93b913245f2e7e4dad112727
esraa <e.mostafa@exp-sa.com>
2025-08-27 13:24:39 +0300

    update menu icons

---
+++ odex25_base/trahum_apps_icons/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Trahum Icons",
+    "summary": "Update module icons",
+    "version": "14.0.1.3.4",
+    "development_status": "Production/Stable",
+    "category": "",
+    "license": "AGPL-3",
+    "application": True,
+    "installable": True,
+    "depends": [
+        "calendar", "mail", "contacts", "mass_mailing", "website_slides", "stock", "exp_hr_payroll", "maintenance",
+        "hr_expense_petty_cash", "hr_expense", "contract", "im_livechat", "account_budget_custom",
+        "system_dashboard_classic", "project", "purchase", "hr_recruitment", "sale_management", "hr_timesheet",
+        "website"
+
+    ],
+    "data": [
+        "views/calender_views.xml",
+    ],
+}
